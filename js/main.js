10c09d19e2b42ceccf9514f8560cdbcf8a11d276
Moti <thatboymoti@gmail.com>
2023-12-02 19:40:52 +0300

    intro and content update

--- js/main.js
+++ js/main.js
@@ -1,117 +1,93 @@
 const intro = document.querySelector('#intro');
-const displayName = document.querySelector('#displayName');
+const greetingText = document.querySelector('#greetingText')
+const nameDisplay = document.querySelector('#nameDisplay');
+const turningGear = document.querySelector('#turningGear')
 
-// changing background color and text color of intro
-setTimeout(() => {
-    intro.classList.remove('bg-black-subtle');
-    intro.classList.remove('text-white');
-    intro.classList.add('bg-light');
-    intro.classList.add('text-dark');
-}, 4000);
-
-displayIntro();
+window.onload = () => {
+    turningGear.classList.add('d-none')
+    greetingText.classList.remove('d-none')
+    displayIntro()
+    setTimeout(() => {
+        intro.classList.remove('bg-black-subtle');
+        intro.classList.remove('text-white');
+        intro.classList.add('bg-light');
+        intro.classList.add('text-dark');
+    }, 4000);
+}
 
 function displayIntro() {
     setTimeout(() => {
-        displayName.innerHTML = '<br>';
+        nameDisplay.innerHTML = '<br>';
     },250);
     setTimeout(() => {
-        displayName.innerHTML = 'e';
+        nameDisplay.innerHTML = 'e';
     },500);
     setTimeout(() => { 
-        displayName.innerHTML += 'c';
+        nameDisplay.innerHTML += 'c';
     },700);
     setTimeout(() => {
-        displayName.innerHTML += 'h';
+        nameDisplay.innerHTML += 'h';
     },900);
     setTimeout(() => {
-        displayName.innerHTML += 'o';
+        nameDisplay.innerHTML += 'o';
     },1100);
     setTimeout(() => {
-        displayName.innerHTML += ' $';
+        nameDisplay.innerHTML += ' $';
     },1300);
     setTimeout(() => {
-        displayName.innerHTML += 'n';
+        nameDisplay.innerHTML += 'n';
     },1500);
     setTimeout(() => {
-        displayName.innerHTML += 'a';
+        nameDisplay.innerHTML += 'a';
     },1700);
     setTimeout(() => {
-        displayName.innerHTML += 'm';
+        nameDisplay.innerHTML += 'm';
     },1900);
     setTimeout(() => {
-        displayName.innerHTML += 'r';
+        nameDisplay.innerHTML += 'r';
     },2100);
     setTimeout(() => {
-        displayName.innerHTML += ';';
-        displayName.classList.add('error-code');
+        nameDisplay.innerHTML += ';';
+        nameDisplay.classList.add('error-code');
     },2300);        
     setTimeout(() => {
         // remove error effects
-        displayName.classList.add('text-danger');
-        displayName.classList.remove('error-code');
-        displayName.innerHTML = 'undefined';
+        nameDisplay.classList.add('text-danger');
+        nameDisplay.classList.remove('error-code');
+        nameDisplay.innerHTML = 'undefined';
     },2700);
     setTimeout(() => {
-        displayName.classList.remove('text-danger');
-        displayName.innerHTML = 'ec';
+        nameDisplay.classList.remove('text-danger');
+        nameDisplay.innerHTML = 'ec';
     },3500);
     setTimeout(() => {
-        displayName.innerHTML += 'ho';
+        nameDisplay.innerHTML += 'ho';
     },3600);
     setTimeout(() => {
-        displayName.innerHTML += ' $';
+        nameDisplay.innerHTML += ' $';
     },3700);
     setTimeout(() => {
-        displayName.innerHTML += 'name';
+        nameDisplay.innerHTML += 'name';
     },3800);
     setTimeout(() => {
-        displayName.innerHTML += ';';
+        nameDisplay.innerHTML += ';';
     },3900);
     setTimeout(() => {
-        displayName.innerHTML = '';
+        // changeIntroColor();
+        nameDisplay.innerHTML = '';
         displayMyName();
-    },4500);
+    },4300);
 }
 
-function displayMyName() {
+const displayMyName = () => {
     let myName = ['M', 'o', 't', 'i', ' A', 'b', 'e', 'b', 'e'];
 
     let i = 0;
     let intervalId = setInterval(() => {
-        displayName.innerHTML += myName[i];
+        nameDisplay.innerHTML += myName[i];
         i++;
         if (i == myName.length) {
             clearInterval(intervalId);
         }
     }, 200);
 }
-
-// const home = document.querySelector('#home');
-// const about = document.querySelector('p');
-// const skills = document.querySelector('#skills');
-
-// // isinviewport function
-// function isInViewport(element) {
-//     const rect = element.getBoundingClientRect();
-//     return (
-//       rect.top >= 0 &&
-//       rect.left >= 0 &&
-//       rect.bottom <= window.innerHeight &&
-//       rect.right <= window.innerWidth
-//     );
-//   }
-
-// // add animation function
-// const addAnimation = () => {
-//     if (isInViewport(home)) {
-//         console.log('in viewport');
-//         about.classList.add('animate__animated');
-//         about.classList.add('animate__zoomIn');
-//     } else {
-//         about.classList.remove('animate__animated');
-//         about.classList.remove('animate__zoomIn');
-//     }    
-// }
-
-// window.addEventListener('scroll', addAnimation);
